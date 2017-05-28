caa73dc78645852746921e5b4e737e1a8d725705
Shiva YB <shiva_yb@apple.com>
2017-05-28 11:53:13 +0530

    fixed a bug

--- src/reducers/tree.js
+++ src/reducers/tree.js
@@ -1,32 +1,6 @@
 import { RECEIVE_TREE, FILTER_VIEW } from '../constants/ActionTypes';
 
 const filter = (root, str) => {
-	// if (!!node.description.match(str)) {
-	// 	node["show"] = true;
-	// 	return [node];
-	// } else {
-	// 	node["show"] = false;
-	// }
-
-	// if (!node.children.length) {
-	// 	node["show"] = false;
-	// 	return [];
-	// }
-	
-	// for (let i=0; i<node.children.length; i++) {
-	// 	let child = node.children[i];
-	// 	let { node: { description }} = child;
-	// 	let result = filter(child, str);
-	// 	if (result && result.length) {
-	// 		result["show"] = true;
-	// 	} else {
-	// 		result["show"] = false;
-	// 	}
-	// }
-	// if (node["show"] === void 0) {
-	// 	node["show"] = false;
-	// }
-	// return [node];
 	function setFlag ({ node }) {
 		callback(node, str);
 		node.children.forEach(setFlag);
@@ -56,13 +30,9 @@ const tree = (state = [], action) => {
 			];
 		case FILTER_VIEW:
 			let { filterText } = action;
-			// if (filterText !== 0 && !filterText) {
-			// 	return state;
-			// }
 			let stateCopy = state.slice();
 			let root = { "node": { id: "0", description: "root", children: stateCopy }};
 			filter(root, filterText);
-			// result = result.filter(arr => !!arr.length).map(arr => ({ "node": arr[0] }));
 			return stateCopy;
 		default:
 			return state;
@@ -78,21 +48,27 @@ const findFilteredNodes = ({ node }) => {
 		return [];
 	}
 	
+	let nodes = [];
 	for (let i=0; i<node.children.length; i++) {
 		let child = node.children[i];
 		let { node: { description }} = child;
 		let result = findFilteredNodes(child);
 		if (result && result.length) {
-			return result;
+			nodes.push.apply(nodes, result);
 		}
 	}
-	return [];
+	return nodes;
 };
 
 export const getFilteredNodes = state => {
 	let tree = {"node": {id: "0", description: "root", children: state}};
 	let result = state.map(node => findFilteredNodes(node));
-	return result.filter(arr => !!arr.length).map(arr => ({ "node": arr[0] }));
+	let filteredArr = result.filter(arr => !!arr.length);
+	let nodes = [];
+	filteredArr.forEach(nodeArr => {
+		nodes.push(...nodeArr.map(arr => ({ "node": arr })));
+	})
+	return nodes;
 }
 
 export default tree;
